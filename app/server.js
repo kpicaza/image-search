9bbd7a049395c964c1bae3e90a6ffe8589561eb3
kpicaza <kpicaza.sound@gmail.com>
2017-08-13 15:57:29 +0200

    add homepage to server

--- app/server.js
+++ app/server.js
@@ -1,9 +1,12 @@
 var env = require('node-env-file');
 var express = require('express');
+var homePageAction = require('./image-search/application/home-page-action');
 
 env(__dirname + '/../.env');
 
 var app = express();
+
+app.get('/', homePageAction);
 
 var listener = app.listen(process.env.PORT || 4200, function () {
   console.log('Your app is listening on port ' + listener.address().port);
