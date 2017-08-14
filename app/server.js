37d0894f8d7634221a2c5f6b2e121ba73d8a5cb5
kpicaza <kpicaza.sound@gmail.com>
2017-08-14 03:31:00 +0200

    integrate server with container

--- app/server.js
+++ app/server.js
@@ -1,12 +1,14 @@
 var env = require('node-env-file');
 var express = require('express');
-var homePageAction = require('./image-search/application/home-page-action');
+var container = require('./container');
 
 env(__dirname + '/../.env');
 
 var app = express();
 
-app.get('/', homePageAction);
+app.get('/', container['HomePageAction']().invoke);
+app.get('/api/images', container['MakeSearchAction']().invoke);
+app.get('/api/recent-searches', container['ObtainRecentSearchAction']().invoke);
 
 var listener = app.listen(process.env.PORT || 4200, function () {
   console.log('Your app is listening on port ' + listener.address().port);
