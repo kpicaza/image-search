00dbf35bab4823fc8a756eb2cd723b7a0e7abe61
kpicaza <kpicaza.sound@gmail.com>
2017-08-14 03:37:41 +0200

    define recent search response format

--- app/image-search/application/obtain-recent-search-action.js
+++ app/image-search/application/obtain-recent-search-action.js
@@ -2,7 +2,15 @@ function ObtainRecentSearchAction(store) {
 
   this.invoke = function (request, response) {
     store.lastTenSearches().then(function (data) {
-      response.json(data);
+
+      response.json(data.map(function (event) {
+        return {
+          query: event.data.query,
+          offset: event.data.offset,
+          ocurred_on: event.occurredOn
+        };
+      }));
+
     }).catch(function (e) {
       response.status(400).json({ error: 'Invalid arguments.' });
     });
