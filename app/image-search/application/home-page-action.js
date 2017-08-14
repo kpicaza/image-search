6c98329ebdd10bd803fe92a93866ca83ec91367b
kpicaza <kpicaza.sound@gmail.com>
2017-08-14 03:27:14 +0200

    update home page action

--- app/image-search/application/home-page-action.js
+++ app/image-search/application/home-page-action.js
@@ -1,10 +1,18 @@
 var path = require('path');
 var pug = require('pug');
 
-module.exports = function (request, response) {
-  var view = pug.compileFile(path.resolve('views/index.pug'));
+function HomePageAction() {
 
-  response.send(view({
-    siteUrl: process.env.SITE_URL
-  }));
-};
+  this.invoke = function (request, response) {
+
+    var view = pug.compileFile(path.resolve('views/index.pug'));
+
+    response.send(view({
+      siteUrl: process.env.SITE_URL
+    }));
+
+  };
+
+}
+
+module.exports = HomePageAction;
